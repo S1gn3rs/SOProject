3fb0d9e1f91af9b0dd64916b6063fd998ce70354
MartimAfonso <martimrafonso@tecnico.ulisboa.pt>
2024-12-28 00:20:24 +0000

    .. Co-authored-by: Guilherme Silva <S1gn3rs@users.noreply.github.com>

--- Project2/proj_SO24-25-p2/src/client/api.c
+++ Project2/proj_SO24-25-p2/src/client/api.c
@@ -3,14 +3,13 @@
 #include "src/common/protocol.h"
 #include <fcntl.h>
 
-char _req_pipe_path[MAX_PIPE_PATH_LENGTH + 1];
-char _resp_pipe_path[MAX_PIPE_PATH_LENGTH + 1];
-char _notif_pipe_path[MAX_PIPE_PATH_LENGTH + 1];
+char api_req_pipe_path[MAX_PIPE_PATH_LENGTH + 1];
+char api_resp_pipe_path[MAX_PIPE_PATH_LENGTH + 1];
+char api_notif_pipe_path[MAX_PIPE_PATH_LENGTH + 1];
 
-int _server_pipe_fd;
-int _req_pipe_fd;
-int _resp_pipe_fd;
-int _notif_pipe_fd;
+int api_req_pipe_fd;
+int api_resp_pipe_fd;
+int api_notif_pipe_fd;
 
 // create pipes and connect
 int kvs_connect(char const* req_pipe_path, char const* resp_pipe_path, char const* server_pipe_path,
@@ -18,87 +17,110 @@ int kvs_connect(char const* req_pipe_path, char const* resp_pipe_path, char cons
 
   char connection_buffer[1 + (MAX_PIPE_PATH_LENGTH + 1) * 3];
   char *cur_buffer_pos;
+  int server_pipe_fd;
+
+  char response_buffer[2];
+  int interrupted_read;
 
   unlink(req_pipe_path);
   unlink(resp_pipe_path);
   unlink(notif_pipe_path);
 
   if (mkfifo(req_pipe_path, 0666) < 0){
-    perror("Couldn't create request pipe.\n");
+    perror("Couldn't create request pipe.");
     return 1;
   }
 
   if (mkfifo(resp_pipe_path, 0666) < 0){
     unlink(req_pipe_path);
-    perror("Couldn't create response pipe.\n");
+    perror("Couldn't create response pipe.");
     return 1;
   }
 
   if (mkfifo(notif_pipe_path, 0666) < 0){
     unlink(req_pipe_path);
     unlink(resp_pipe_path);
-    perror("Couldn't create notifications pipe.\n");
+    perror("Couldn't create notifications pipe.");
     return 1;
   }
 
-  if ((_server_pipe_fd = open(server_pipe_path, O_WRONLY)) < 0){
-    perror("Couldn't open server pipe.\n");
+  if ((server_pipe_fd = open(server_pipe_path, O_WRONLY)) < 0){
+    perror("Couldn't open server pipe.");
     unlink(req_pipe_path);
     unlink(resp_pipe_path);
     unlink(notif_pipe_path);
     return 1;
   }
+
   cur_buffer_pos = connection_buffer;
   *cur_buffer_pos = OP_CODE_CONNECT; ////////////////////////////////////////////////////// É PRECISO DAR HANDLE DO ERRO DO STRNCPY?
   cur_buffer_pos++;
 
-  strncpy(_req_pipe_path, req_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
+  strncpy(api_req_pipe_path, req_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
   strncpy(cur_buffer_pos, req_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
   cur_buffer_pos += MAX_PIPE_PATH_LENGTH + 1;
 
-  strncpy(_resp_pipe_path, resp_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
+  strncpy(api_resp_pipe_path, resp_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
   strncpy(cur_buffer_pos, resp_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
   cur_buffer_pos += MAX_PIPE_PATH_LENGTH + 1;
 
-  strncpy(_notif_pipe_path, notif_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
+  strncpy(api_notif_pipe_path, notif_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
   strncpy(cur_buffer_pos, notif_pipe_path, MAX_PIPE_PATH_LENGTH + 1);
 
-  if (write_all(_server_pipe_fd, connection_buffer, 1 + (MAX_PIPE_PATH_LENGTH + 1) * 3) < 0){
+  if (write_all(server_pipe_fd, connection_buffer, 1 + (MAX_PIPE_PATH_LENGTH + 1) * 3) < 0){
+    close(server_pipe_fd);
     unlink(req_pipe_path);
     unlink(resp_pipe_path);
     unlink(notif_pipe_path);
-    close(_server_pipe_fd);
     return 1;
   }
 
-  if ((_req_pipe_fd = open(server_pipe_path, O_WRONLY)) < 0){
-    perror("Couldn't open client request pipe.\n");
+  close(server_pipe_fd);
+
+  if ((api_req_pipe_fd = open(req_pipe_path, O_WRONLY)) < 0){
+    perror("Couldn't open client request pipe.");
     unlink(req_pipe_path);
     unlink(resp_pipe_path);
     unlink(notif_pipe_path);
-    close(_server_pipe_fd);
     return 1;
   }
 
-  if ((_resp_pipe_fd = open(server_pipe_path, O_RDONLY)) < 0){ //ver se n bloqueia e se tem de ser rdwr
-    perror("Couldn't open client response pipe.\n");
+  if ((api_resp_pipe_fd = open(resp_pipe_path, O_RDONLY)) < 0){ //ver se n bloqueia e se tem de ser rdwr
+    perror("Couldn't open client response pipe.");
+    close(api_req_pipe_fd);
     unlink(req_pipe_path);
     unlink(resp_pipe_path);
     unlink(notif_pipe_path);
-    close(_server_pipe_fd);
-    close(_req_pipe_fd);
     return 1;
   }
 
-  if ((_notif_pipe_fd = open(server_pipe_path, O_RDONLY)) < 0){ //ver se n bloqueia e se tem de ser rdwr
-    perror("Couldn't open client notification pipe.\n");
+  if ((api_notif_pipe_fd = open(notif_pipe_path, O_RDONLY)) < 0){ //ver se n bloqueia e se tem de ser rdwr
+    perror("Couldn't open client notification pipe.");
+    close(api_req_pipe_fd);
+    close(api_resp_pipe_fd);
     unlink(req_pipe_path);
     unlink(resp_pipe_path);
     unlink(notif_pipe_path);
-    close(_server_pipe_fd);
-    close(_req_pipe_fd);
-    close(_resp_pipe_fd);
     return 1;
+  }
+
+  if (read_all(api_resp_pipe_fd, response_buffer, 2, &interrupted_read) < 0){
+    perror("Couldn't read message from server.");
+    close(api_req_pipe_fd);
+    close(api_resp_pipe_fd);
+    unlink(req_pipe_path);
+    unlink(resp_pipe_path);
+    unlink(notif_pipe_path);
+  }
+
+  if(response_buffer[1] == '1'){
+    perror("Couldn't connect to server.");
+    close(api_req_pipe_fd);
+    close(api_resp_pipe_fd);
+    unlink(req_pipe_path);
+    unlink(resp_pipe_path);
+    unlink(notif_pipe_path);
+    return 0;
   }
 
   return 0;
@@ -106,29 +128,31 @@ int kvs_connect(char const* req_pipe_path, char const* resp_pipe_path, char cons
 
 // close pipes and unlink pipe files
 int kvs_disconnect(void) {
+  char response_buffer[2];
+  int interrupted_read;
 
-  if (write_all(_server_pipe_fd, (char) OP_CODE_DISCONNECT, 1 ) < 0)
+  if (write_all(api_req_pipe_fd, OP_CODE_DISCONNECT, 1) < 0)
     return 1;
-
-  unlink(_req_pipe_path);
-  unlink(_resp_pipe_path);
-  unlink(_notif_pipe_path);
-  close(_server_pipe_fd);
-  close(_req_pipe_fd);
-  close(_resp_pipe_fd);
-  close(_notif_pipe_fd);
+  close(api_req_pipe_fd);
+  close(api_resp_pipe_fd);
+  close(api_notif_pipe_fd);
+  unlink(api_req_pipe_path);
+  unlink(api_resp_pipe_path);
+  unlink(api_notif_pipe_path);
   return 0;
 }
 
 
 // send subscribe message to request pipe and wait for response in response pipe
 int kvs_subscribe(const char* key) {
-  char subscribe_buffer[MAX_STRING_SIZE + 2]; // Não sei se devemos colocar 1 ou 2 por causa do null terminator da key
+  char subscribe_buffer[MAX_STRING_SIZE + 2];
+  char response_buffer[2];
+  int interrupted_read;
 
   *subscribe_buffer = OP_CODE_SUBSCRIBE;
-  strncpy(subscribe_buffer + 1, key, MAX_STRING_SIZE + 1); // Não sei se devemos colocar 1 ou 2 por causa do null terminator da key
+  strncpy(subscribe_buffer + 1, key, MAX_STRING_SIZE + 1);
 
-  if (write_all(_server_pipe_fd, subscribe_buffer, MAX_STRING_SIZE + 2) < 0)
+  if (write_all(api_req_pipe_fd, subscribe_buffer, MAX_STRING_SIZE + 2) < 0)
     return 1;
   return 0;
 }
@@ -136,12 +160,14 @@ int kvs_subscribe(const char* key) {
 
 // send unsubscribe message to request pipe and wait for response in response pipe
 int kvs_unsubscribe(const char* key) {
-  char unsubscribe_buffer[MAX_STRING_SIZE + 2]; // Não sei se devemos colocar 1 ou 2 por causa do null terminator da key
+  char unsubscribe_buffer[MAX_STRING_SIZE + 2];
+  char response_buffer[2];
+  int interrupted_read;
 
   *unsubscribe_buffer = OP_CODE_UNSUBSCRIBE;
-  strncpy(unsubscribe_buffer + 1, key, MAX_STRING_SIZE + 1); // Não sei se devemos colocar 1 ou 2 por causa do null terminator da key
+  strncpy(unsubscribe_buffer + 1, key, MAX_STRING_SIZE + 1);
 
-  if (write_all(_server_pipe_fd, unsubscribe_buffer, MAX_STRING_SIZE + 2) < 0)
+  if (write_  (api_req_pipe_fd, unsubscribe_buffer, MAX_STRING_SIZE + 2) < 0)
     return 1;
   return 0;
 }
