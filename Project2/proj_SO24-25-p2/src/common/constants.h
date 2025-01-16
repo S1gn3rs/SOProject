f28fd497b43ba0b6e690850aa2853e8c098333e6
Guilherme <guilherme8888silva@gmail.com>
2025-01-16 08:31:41 +0000

    last change

--- Project2/proj_SO24-25-p2/src/common/constants.h
+++ Project2/proj_SO24-25-p2/src/common/constants.h
@@ -1,6 +1,6 @@
 // constantes partilhadas entre cliente e servidor
-#define MAX_SESSION_COUNT 8  // num max de sessoes no server
+#define MAX_SESSION_COUNT 2  // num max de sessoes no server
 #define STATE_ACCESS_DELAY_US  // delay a aplicar no server
 #define MAX_PIPE_PATH_LENGTH 40 // tamanho max do caminho do pipe
 #define MAX_STRING_SIZE 40
-#define MAX_NUMBER_SUB 10
+#define MAX_NUMBER_SUB 3
